6c7b101b607f9414c0531aae9d09e341ac029cc2
Wee Jian Hui Kevin <kevin@Wees-MacBook-Air.local>
2024-01-25 11:13:03 +0800

    feat: add new buttons for ease of testing

--- src/initialPageLoad.js
+++ src/initialPageLoad.js
@@ -1,8 +1,24 @@
-import { createElement } from './utils.js';
+import { createElement, appendMultipleChildren } from './utils.js';
+import UpdateView from './updateView.js';
+import RemoveQuest from './removeQuest.js';
+import RemoveTask from './removeTask.js';
+import AddQuest from './addQuest.js';
+import AddTask from './addTask.js';
+import EditTask from './editTask.js';
 
 export default () => {
     const container = document.querySelector('body');
-    const starter = createElement('h1', ['text-2xl', 'font-semibold', 'text-red-100'], 'hello, world');
-    
-    container.appendChild(starter);
+    const updateViewBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Update View');
+    const removeQuestBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Remove Quest');
+    const removeTaskBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Remove Task');
+    const addQuestBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Add Quest');
+    const addTaskBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Add Task');
+    const editTaskBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Edit Task');
+    updateViewBtn.addEventListener('click', () => UpdateView());
+    removeQuestBtn.addEventListener('click', () => RemoveQuest())
+    removeTaskBtn.addEventListener('click', () => RemoveTask());
+    addQuestBtn.addEventListener('click', () => AddQuest())
+    addTaskBtn.addEventListener('click', () => AddTask());
+    editTaskBtn.addEventListener('click', () => EditTask());
+    appendMultipleChildren(container, updateViewBtn, removeQuestBtn, removeTaskBtn, addQuestBtn, addTaskBtn, editTaskBtn);    
 }
