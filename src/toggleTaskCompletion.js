dad20ffb2dcb66516cd42948996e6dfb97f98ecb
Wee Jian Hui Kevin <kevin@Wees-MacBook-Air.local>
2024-01-26 11:25:08 +0800

    refactor: use quest and task id to select elements to toggle completion

--- src/toggleTaskCompletion.js
+++ src/toggleTaskCompletion.js
@@ -1,34 +1,27 @@
 import UpdateView from './updateView.js';
 
-export default () => {
+export default (questId, taskId) => {
     const initialQuestLog = JSON.parse(localStorage.getItem('questLog'));
     const revisedQuestLog = initialQuestLog.slice();
-    const questNames = initialQuestLog.map((quest) => quest['name']);
-    const questName = prompt('Quest to edit: ')
 
-    if (questNames.includes(questName)) {
-        const affectedQuest = revisedQuestLog.find((quest) => quest.name == questName)
-        const affectedTaskTitle = prompt('Task to edit:')
-        
-        const affectedTask = affectedQuest.tasks.find((task) => task.title == affectedTaskTitle);
-        console.log('affected quest is ', affectedQuest);
-        console.log('affected task is ', affectedTask);
+    const affectedQuest = revisedQuestLog.find((quest) => quest.id == questId)
+    
+    const affectedTask = affectedQuest.tasks.find((task) => task.id == taskId);
+    console.log('affected quest is ', affectedQuest);
+    console.log('affected task is ', affectedTask);
 
-        if (affectedTask) {
-            const newTask = {
-                ...affectedTask,
-                'completed': !affectedTask.completed,
-            }
-            console.log('time to filter', affectedQuest);
-            affectedQuest.tasks = affectedQuest.tasks.filter((task) => task.title !== affectedTaskTitle)
-            console.log('after filtering', affectedQuest);
-            affectedQuest.tasks.push(newTask);
-            console.log('after pushing', affectedQuest);
-            localStorage.setItem('questLog', JSON.stringify(revisedQuestLog));
-            UpdateView();
-            return;
+    if (affectedTask) {
+        const newTask = {
+            ...affectedTask,
+            'completed': !affectedTask.completed,
         }
-        console.error('Invalid task name');
+        console.log('time to filter', affectedQuest);
+        affectedQuest.tasks = affectedQuest.tasks.filter((task) => task.id !== taskId)
+        console.log('after filtering', affectedQuest);
+        affectedQuest.tasks.push(newTask);
+        console.log('after pushing', affectedQuest);
+        localStorage.setItem('questLog', JSON.stringify(revisedQuestLog));
+        UpdateView();
+        return;
     }
-    console.error('Invalid quest name');
 }
