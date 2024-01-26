eb06eaeb5bd8d3cdb88883467df38eedb8a3ee56
Wee Jian Hui Kevin <kevinweejh@gmail.com>
2024-01-26 11:45:40 +0800

    refactor: remove unnecessary test btns

--- src/buildQuestList.js
+++ src/buildQuestList.js
@@ -1,5 +1,9 @@
 import { createElement, appendMultipleChildren } from './utils.js';
+import AddTask from './addTask.js';
 import EditTask from './editTask.js';
+import RemoveTask from './removeTask.js';
+import RemoveQuest from './removeQuest.js';
+import ToggleTaskCompletion from './toggleTaskCompletion.js';
 
 export default () => {
     const container = document.querySelector('body');
@@ -8,24 +12,32 @@ export default () => {
     questLog.forEach((quest) => {
         const questContainer = createElement('div', ['flex', 'flex-col', 'border', 'border-blue-300', 'border-4'], '');
         const questName = createElement('h1', ['text-2xl', 'font-semibold', 'border'], `Quest: ${quest.name}`);
+        const questAddTaskBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg', 'w-fit'], '+ Task');
+        questAddTaskBtn.addEventListener('click', () => AddTask(quest.id));
+        const questRemoveBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg', 'w-fit'], 'x');
+        questRemoveBtn.addEventListener('click', () => RemoveQuest(quest.id));
         const questDefaultStatus = createElement('p', ['border'], `Default: ${quest.default}`);
-        appendMultipleChildren(questContainer, questName, questDefaultStatus);
+        
+        appendMultipleChildren(questContainer, questName, questAddTaskBtn, questRemoveBtn, questDefaultStatus);
 
         quest.tasks.forEach((task) => {
             const taskContainer = createElement('div', ['flex', 'flex-col', 'border', 'border-red-300', 'border-4'], '');
             const taskTitle = createElement('h1', ['text-2xl', 'font-semibold', 'border', 'border-red-300', 'border-4'], `Title: ${task.title}`);
             const taskEditBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg', 'w-fit'], '...');
             taskEditBtn.addEventListener('click', () => EditTask(quest.id, task.id));
+            const taskRemoveBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg', 'w-fit'], 'x');
+            taskRemoveBtn.addEventListener('click', () => RemoveTask(quest.id, task.id));
+            const taskToggleCompletionBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg', 'w-fit'], '✓');
+            taskToggleCompletionBtn.addEventListener('click', () => ToggleTaskCompletion(quest.id, task.id));
             const taskDescription = createElement('p', ['border', 'border-red-300', 'border-4'], `Description: ${task.description}`);
             const taskPriority = createElement('p', ['border', 'border-red-300', 'border-4'], `Priority: ${task.priority}`);
             const taskDueDate = createElement('p', ['border', 'border-red-300', 'border-4'], `Due date: ${task.dueDate}`);
-            const taskCompletionStatus = createElement('p', ['border', 'border-red-300', 'border-4'], `Completed: ${task.completed}`);
-            appendMultipleChildren(taskContainer, taskTitle, taskEditBtn, taskDescription, taskPriority, taskDueDate, taskCompletionStatus)
+            
+            appendMultipleChildren(taskContainer, taskTitle, taskEditBtn, taskRemoveBtn, taskToggleCompletionBtn, taskDescription, taskPriority, taskDueDate)
 
             questContainer.appendChild(taskContainer);
         })
         questList.appendChild(questContainer);
-        
     })
     container.appendChild(questList);
 }
--- src/populateTestBtns.js
+++ src/populateTestBtns.js
@@ -1,29 +1,14 @@
 import { createElement, appendMultipleChildren } from './utils.js';
 import UpdateView from './updateView.js';
-import RemoveQuest from './removeQuest.js';
-import RemoveTask from './removeTask.js';
 import AddQuest from './addQuest.js';
-import AddTask from './addTask.js';
-import EditTask from './editTask.js';
-import ToggleTaskCompletion from './toggleTaskCompletion.js';
 
 export default () => {
     const container = document.querySelector('body');
     const updateViewBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Update View');
-    const removeQuestBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Remove Quest');
-    const removeTaskBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Remove Task');
     const addQuestBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Add Quest');
-    const addTaskBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Add Task');
-    const editTaskBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Edit Task');
-    const toggleTaskCompletionBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg'], 'Toggle Task Completion');
     
     updateViewBtn.addEventListener('click', () => UpdateView());
-    removeQuestBtn.addEventListener('click', () => RemoveQuest())
-    removeTaskBtn.addEventListener('click', () => RemoveTask());
     addQuestBtn.addEventListener('click', () => AddQuest())
-    addTaskBtn.addEventListener('click', () => AddTask());
-    editTaskBtn.addEventListener('click', () => EditTask());
-    toggleTaskCompletionBtn.addEventListener('click', () => ToggleTaskCompletion());
     
-    appendMultipleChildren(container, updateViewBtn, removeQuestBtn, removeTaskBtn, addQuestBtn, addTaskBtn, editTaskBtn, toggleTaskCompletionBtn);    
+    appendMultipleChildren(container, updateViewBtn, addQuestBtn);    
 }
