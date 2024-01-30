7df6317d0d452504dc2445ee6c56d78bc76d4a3b
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-01-30 17:43:33 +0800

    style: questAdd btn theme, questList div height

--- src/buildQuestList.js
+++ src/buildQuestList.js
@@ -8,8 +8,8 @@ import ToggleTaskCompletion from './toggleTaskCompletion.js';
 export default () => {
     const container = document.querySelector('body');
     const questLog = JSON.parse(localStorage.getItem('questLog'));
-    const questAddBtn = createElement('btn', ['p-3', 'm-3', 'border', 'hover:bg-purple-600', 'cursor-pointer', 'rounded-lg', 'w-fit', 'z-10', 'absolute', 'top-[5%]'], 'Add Quest');
-    const questList = createElement('div', ['flex', 'flex-col', 'border-green-300', 'border-4', 'z-10', 'absolute', 'max-h-[75%]', 'overflow-y-auto', 'top-1/2', 'left-1/2', 'transform', '-translate-x-1/2', '-translate-y-1/2'], '', 'fullQuestLog');
+    const questAddBtn = createElement('btn', ['p-3', 'm-3', 'border-brown-600', 'border-2', 'text-white', 'hover:bg-yellow-400', 'hover:border-yellow-400', 'hover:text-yellow-800', 'cursor-pointer', 'rounded-full', 'w-1/3', 'text-center', 'bg-yellow-800', 'z-10', 'absolute', 'top-[5%]', 'font-extrabold'], 'Add Quest');
+    const questList = createElement('div', ['flex', 'flex-col', 'border-green-300', 'border-4', 'z-10', 'absolute', 'max-h-[65%]', 'overflow-y-auto', 'top-1/2', 'left-1/2', 'transform', '-translate-x-1/2', '-translate-y-1/2', 'w-1/3'], '', 'fullQuestLog');
     questLog.forEach((quest) => {
         const questContainer = createElement('div', ['flex', 'flex-col', 'border', 'border-blue-300', 'border-4'], '');
         const questName = createElement('h1', ['text-2xl', 'font-semibold', 'border'], `Quest: ${quest.name}`);
