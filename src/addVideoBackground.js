589155e46ac5a9585469c90386a4d5924818d2d0
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-01-30 12:37:45 +0800

    style: add black overlay between video and content

--- src/addVideoBackground.js
+++ src/addVideoBackground.js
@@ -1,10 +1,12 @@
-import { createElement } from './utils.js';
+import { createElement, appendMultipleChildren } from './utils.js';
 import questLogBackground from '/src/questLogBackground.webm';
 
 export default () => {
     const container = document.querySelector('body');
 
-    const video = createElement('video', ['fixed', 'right-0', 'bottom-0', 'min-w-full', 'max-w-full', 'w-auto', 'h-auto', 'z-0', 'bg-cover', 'overflow-hidden'],'');
+    const bgLayer = createElement('div', ['absolute', 'w-screen', 'h-screen', 'overflow-hidden'], '');
+
+    const video = createElement('video', ['absolute', 'right-0', 'bottom-0', 'min-w-full', 'min-h-full', 'w-auto', 'h-auto', '-z-10', 'object-cover'],'');
     video.autoplay = true;
     video.loop = true;
     video.muted = true;
@@ -15,5 +17,9 @@ export default () => {
 
     video.appendChild(source);
 
-    container.appendChild(video);
+    const overlay = createElement('div', ['absolute', 'top-0', 'left-0', 'w-full', 'h-full', 'object-cover', 'z-0', 'bg-black/50'], '');
+
+    appendMultipleChildren(bgLayer, video, overlay);
+
+    container.appendChild(bgLayer);
 }
